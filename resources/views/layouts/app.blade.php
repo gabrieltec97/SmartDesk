6ee9340f09899c1ccb311a94a03197b7bfd8ee5a
Gabriel <gabrielphp20@gmail.com>
2025-08-10 11:29:08 -0300

    teste de commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="fa-solid fa-chart-line {{ Route::is('dashboard') ? '' : 'format-color' }} sidenav-icon"></i>
                     </div>
-                    <span class="nav-link-text ms-1 font-weight-bold">Dashboard</span>
+                    <span class="nav-link-text ms-1 font-weight-bold">Dashboards</span>
                 </a>
             </li>
             <li class="nav-items">
