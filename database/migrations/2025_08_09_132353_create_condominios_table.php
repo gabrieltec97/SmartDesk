f620ef9f2e240dc7ad8f8c0003f87e4af3e877dc
Gabriel <gabrielphp20@gmail.com>
2025-08-11 15:10:18 -0300

    Iniciando a estrutura das tabelas de funcionario e condomínios

--- database/migrations/2025_08_09_132353_create_condominios_table.php
+++ database/migrations/2025_08_09_132353_create_condominios_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('condominios', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('localizacao');
             $table->timestamps();
         });
     }
