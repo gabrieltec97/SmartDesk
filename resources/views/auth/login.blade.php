f76c271f32f47ee1623ff4b4fe2ed27f0c8a75e0
Gabriel <gabrielphp20@gmail.com>
2025-08-26 17:43:18 -0300

    Reajuste do layout da tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Login - E-Locker entregas</title>
+    <title>Login - EasyOs - Chamados Inteligentes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
     <link sizes="76x76" href="../assets/img/e-locker.png">
